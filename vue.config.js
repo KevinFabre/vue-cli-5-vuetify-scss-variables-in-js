7837eaf5b4f0b112d0a7bb9221c69ae42e20e25f
Kevin Fabre <me@kevinfabre.com>
2022-03-29 14:03:34 +0200

    [FIXME] Vuetify stylesheets not generated

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  // Comment to generate Vuetify stylesheets
+  css: {
+    loaderOptions: {
+      css: {
+        modules: {
+          auto: () => true,
+        },
+      },
+    },
+  },
 });
