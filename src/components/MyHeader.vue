55ec5592ac43d80b3c4303438d21e56fb7483b0c
Tony Trinh <tony19@gmail.com>
2022-04-11 22:10:36 -0500

    add 2nd component that uses app.module.scss

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 :style="{ color: mediumcolor }"><slot /></h2>
+</template>
+
+<script>
+import { mediumcolor } from "@/styles/app.module.scss";
+
+export default {
+  data() {
+    return {
+      mediumcolor,
+    };
+  },
+};
+</script>
